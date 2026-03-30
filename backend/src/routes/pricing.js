e32dabfba459d4a468904522c59ed7dda0b71362
shrawani1619 <kaleshrawani16@gmail.com>
2026-03-30 16:16:56 +0530

    Revert "Initial commit"
    This reverts commit d4c4501107ef0ae4ef5f6e93520f6c668393a849.

--- backend/src/routes/pricing.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { estimate } from '../controllers/pricing.controller.js'
-
-const router = Router()
-
-router.post('/estimate', estimate)
-
-export default router
-
